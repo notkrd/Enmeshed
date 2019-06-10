a73f5c55b55093afa5d6c390c62d9c56d614c30b
Kavi Duvvoori <notkrd@gmail.com>
2019-06-10 14:25:14 -0700

    messing around with the least important things

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get('/', (req, res) => res.send('Goodbye, cruel world!'))
+const port = 3000;
 
-app.listen(port, () => console.log('Whatever forests might previously have existed were made illegible by several thousand years\' continuous human habitation.'))
+app.use(express.static('static'));
+
+io.on('connection', (socket) => {
+    console.log('What\'re they doing here??');
+    socket.on('a messenger', (msg) => {
+        console.log('they carry: ' + msg);
+        io.emit('a messenger', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log("Must be no one.");
+    });
+});
+
+http.listen(port, () => console.log('Whatever forests might previously have existed were made illegible by several thousand years\' continuous human habitation and logging.'));
---
+++ test/test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('pressupositions', function() {
+    it('Finger arithmetic as a metaphor for certainty', function() {
+        assert.equal(2 + 2, 4);
+    });
+});
